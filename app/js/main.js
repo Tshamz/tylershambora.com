b9a71ce7049b2c99fe22296f549bb3457097b38b
tshamz <tyler@theshamboras.com>
2014-06-28 00:34:02 -0700

    the megaest of all the commits in the history of time

--- app/js/main.js
+++ app/js/main.js
@@ -5,58 +5,183 @@ $(document).ready(function() {
 ==========================================================================*/
 
   yepnope({
-    load: 'js/feature-test.min.js'
+    load: 'js/feature-test.min.js',
   });
 
 /*==========================================================================
   VARIABLES
 ==========================================================================*/
 
+  var subPagesDocument;
+  var galleryInfos = '';
+  var cachedGalleries = {};
   var messageDelay = 2000;
-  var fadeTransitionLength = 250;
   var slideTransitionLength = 800;
+  var fadeTransitionLength = 400;
   var $guts = $('#guts');
-  var $landing = $('#landing');
-  var subGalleries = ["papn", "csh", "fb", "sns", "petershambora", "wtfdrink", "laura", "resizely", "cpconsulting"];
+  var subGalleries = ["papn", "csh", "fb", "sns", "petershambora", "wtfdrink", "laura", "resizely", "cpsolutions"];
+
+/*==========================================================================
+  ON INITIAL PAGE LOAD
+==========================================================================*/
+
+  $.when(
+    $.get("subPagesDocument.html", function(data) {
+      subPagesDocument = $(data);
+    }),
+    $.getJSON('/js/galleryInfos.json', function(data) {
+      galleryInfos = data;
+    })
+  ).then(function() {
+    initialLoad();
+  });
+
+  var initialLoad = function() {
+    var hash = window.location.hash.substring(1);
+    if (hash === "") {
+      hash = "entrance";
+    }
+    $('#main-header .' + hash).addClass('is-active');
+    $('#main-header .' + hash + ' a').attr('disabled', 'disabled');  // Prevents further clicks on active link in IE
+    loadPageContent(hash, function() {
+      $guts.toggleClass('slideUp slideDown');
+      setTimeout(function(){$('.content').toggleClass('fadeOut fadeIn');}, slideTransitionLength);
+    });
+  };
 
 /*==========================================================================
   LOADING PAGE CONTENT AND GALLERY SETUP
 ==========================================================================*/
 
   var loadPageContent = function(pageName, callback) {
-    $guts.load("sub-pages.html #" + pageName + ", #guts-footer", function() {
-      var newPage = pageName;
-      switch (newPage) {
-        case "resume":
-        case "papn":
-        case "csh":
-        case "fb":
-        case "sns":
-        case "petershambora":
-        case "wtfdrink":
-        case "laura":
-        case "resizely":
-        case "cpsolutions":
-          buildUpGallery();
-          break;
-        case "contact":
-          repopulateForm();
-          break;
-      }
-      if (callback) {
-        callback();
-      }
-    });
+    $guts.children('section').remove();
+    var newPage = pageName;
+    switch (newPage) {
+      case "resume":
+      case "papn":
+      case "csh":
+      case "fb":
+      case "sns":
+      case "petershambora":
+      case "wtfdrink":
+      case "laura":
+      case "resizely":
+      case "cpsolutions":
+        buildUpGallery(newPage);
+        break;
+      case "contact":
+        repopulateForm();
+      default:
+        $guts.prepend(subPagesDocument.closest('#' + newPage));
+    }
+    if (callback) {
+      callback();
+    }
   };
 
   // Build and setup whatever gallery's been loaded
-  var buildUpGallery = function() {
-    $('.content-presenter li').each(function(index) {
-      $(this).css('left', index + '00%');
-    });
-    $('.content-presenter li:first-child').addClass('first');
-    $('.content-presenter li:last-child').addClass('last');
+  var buildUpGallery = function(galleryName) {
+
+    // If the gallery has already been built, don't rebuild it
+    if (galleryName in cachedGalleries) {
+      $guts.prepend(cachedGalleries[galleryName]);
+      return;
+    }
+
+    var galleryStatus = galleryInfos[galleryName]["galleryStatus"];
+    var onlineBadge = '<svg xmlns="http://www.w3.org/2000/svg" width="88" height="18"><linearGradient id="a" x2="0" y2="100%"><stop offset="0" stop-color="#fff" stop-opacity=".7"/><stop offset=".1" stop-color="#aaa" stop-opacity=".1"/><stop offset=".9" stop-opacity=".3"/><stop offset="1" stop-opacity=".5"/></linearGradient><rect rx="4" width="88" height="18" fill="#555"/><rect rx="4" x="44" width="44" height="18" fill="#4c1"/><path fill="#4c1" d="M44 0h4v18h-4z"/><rect rx="4" width="88" height="18" fill="url(#a)"/><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="23" y="13" fill="#010101" fill-opacity=".3">status</text><text x="23" y="12">status</text><text x="65" y="13" fill="#010101" fill-opacity=".3">online</text><text x="65" y="12">online</text></g></svg>';
+    var offlineBadge = '<svg xmlns="http://www.w3.org/2000/svg" width="89" height="18"><linearGradient id="a" x2="0" y2="100%"><stop offset="0" stop-color="#fff" stop-opacity=".7"/><stop offset=".1" stop-color="#aaa" stop-opacity=".1"/><stop offset=".9" stop-opacity=".3"/><stop offset="1" stop-opacity=".5"/></linearGradient><rect rx="4" width="89" height="18" fill="#555"/><rect rx="4" x="44" width="45" height="18" fill="#e05d44"/><path fill="#e05d44" d="M44 0h4v18h-4z"/><rect rx="4" width="89" height="18" fill="url(#a)"/><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="23" y="13" fill="#010101" fill-opacity=".3">status</text><text x="23" y="12">status</text><text x="65.5" y="13" fill="#010101" fill-opacity=".3">offline</text><text x="65.5" y="12">offline</text></g></svg>';
+    var mockupBadge = '<svg xmlns="http://www.w3.org/2000/svg" width="99" height="18"><linearGradient id="a" x2="0" y2="100%"><stop offset="0" stop-color="#fff" stop-opacity=".7"/><stop offset=".1" stop-color="#aaa" stop-opacity=".1"/><stop offset=".9" stop-opacity=".3"/><stop offset="1" stop-opacity=".5"/></linearGradient><rect rx="4" width="99" height="18" fill="#555"/><rect rx="4" x="44" width="55" height="18" fill="#dfb317"/><path fill="#dfb317" d="M44 0h4v18h-4z"/><rect rx="4" width="99" height="18" fill="url(#a)"/><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="23" y="13" fill="#010101" fill-opacity=".3">status</text><text x="23" y="12">status</text><text x="70.5" y="13" fill="#010101" fill-opacity=".3">' + galleryStatus + '</text><text x="70.5" y="12">' + galleryStatus + '</text></g></svg>';
+
+    var pageTitle = galleryInfos[galleryName]["galleryTitle"];
+    var pageDescription = galleryInfos[galleryName]["galleryDescription"];
+    var marqueeImage = galleryInfos[galleryName]["marqueeImage"];
+    var pageURL = galleryInfos[galleryName]["galleryURL"];
+    var pageURLAppearance = galleryInfos[galleryName]["galleryURLAppearance"];
+    var statusBadge = '';
+    if (galleryStatus === 'online') {
+      marqueeImage = '<a href="' + pageURL + '" target="_blank"><img src="' + marqueeImage + '" /></a>';
+      statusBadge = onlineBadge;
+      pageURL = '<p><strong><a href="' + pageURL + '" target="_blank">' + pageURLAppearance + '</a></strong></p>';
+    } else if (galleryStatus === 'offline') {
+      marqueeImage = '<img src="' + marqueeImage + '" />';
+      statusBadge = offlineBadge;
+      pageURL = '';
+    } else {
+      marqueeImage = '<img src="' + marqueeImage + '" />';
+      statusBadge = mockupBadge;
+      pageURL = '';
+    }
+
+    var baseGallery = '';
+    if (galleryInfos[galleryName]["subGallery"]) {
+      baseGallery += '<section class="fadeOut content sub gallery">';
+    } else if (galleryName === "resume") {
+      baseGallery += '<section class="fadeOut content gallery resume">';
+    } else  {
+      baseGallery += '<section class="fadeOut content gallery>';
+    }
+      baseGallery += '<nav>';
+        baseGallery += '<div class="gallery-control previous"></div>';
+        baseGallery += '<div class="gallery-control next"></div>';
+      baseGallery += '</nav>';
+      baseGallery += '<div class="slide" style="left:0%;">';
+        baseGallery += '<div class="gallery-panel marquee">';
+          baseGallery += '<div class="marquee-image">';
+            baseGallery += marqueeImage;
+          baseGallery += '</div>';
+          baseGallery += '<div class="gallery-title">';
+            baseGallery += '<span class="page-title"><h2 class="hyphenate">' + pageTitle + ' </h2><span class="page-badge">' + statusBadge + '</span></span>';
+          baseGallery += '</div>';
+          baseGallery += '<div class="gallery-url">';
+            baseGallery += pageURL;
+          baseGallery += '</div>';
+          baseGallery += '<div class="gallery-description">';
+            baseGallery += '<p>' + pageDescription + '</p>';
+          baseGallery += '</div>';
+        baseGallery += '</div>';
+      baseGallery += '</div>';
+
+    var additionalGalleries = galleryInfos[galleryName]["additionalGalleries"];
+    var additionalGalleryPanels = '';
+    for (i = 0; i < additionalGalleries.length; i++) {
+      var activeGallery = additionalGalleries[i];
+      var lightboxExists = activeGallery[0];
+      var extraClasses = activeGallery[1];
+      var imageLocation = activeGallery[2];
+      var imageDescription = activeGallery[3];
+      var n = i + 1;
+
+      additionalGalleryPanels += '<div class="slide" style="left:' + n + '00%;">';
+        additionalGalleryPanels += '<div class="gallery-panel repeat ' + extraClasses + '">';
+          additionalGalleryPanels += '<div class="gallery-image"><img src="img/sites/' + imageLocation + '" lightbox="' + lightboxExists + '" /></div>';
+          additionalGalleryPanels += '<div class="gallery-text"><p>' + imageDescription + '</p></div>';
+        additionalGalleryPanels += '</div>';
+      additionalGalleryPanels += '</div>';
+    }
+
+    var fullGallery = baseGallery + additionalGalleryPanels + '</section>';
+    cachedGalleries[galleryName] = fullGallery;
+    $guts.prepend(fullGallery);
+
+    $('.gallery .slide:first-of-type').addClass('first');
+    $('.gallery .slide:last-of-type').addClass('last');
     $('.previous').addClass('is-disabled');
+
+    $guts.on('click', '.gallery img[lightbox="true"]', function() {
+      var imgSrc = $(this).attr('src');
+      $('#lightbox-image').attr('src', imgSrc);
+      $('#lightbox').show();
+    });
+    $('#lightbox-image').click(function(e) {
+      e.stopPropagation();
+    });
+    $('#lightbox').click(function() {
+      $('#lightbox').hide();
+    });
+    $('.gallery-panel.resume img').click(function() {
+      window.open('/resume.pdf', 'Tyler Shambora\'s Resume');
+    });
   };
 
 /*==========================================================================
@@ -75,7 +200,7 @@ $(document).ready(function() {
       return false;
     }
     isAnimating = true;
-    $('.content-presenter li').animate({left: "+=100%"}, 750, function() {
+    $('.gallery .slide').animate({left: "+=100%"}, 750, function() {
       isAnimating = false;
       if ($('.first').css('left') === "0px") {
         $('.previous').addClass('is-disabled');
@@ -95,7 +220,7 @@ $(document).ready(function() {
       return false;
     }
     isAnimating = true;
-    $('.content-presenter li').animate({left: "-=100%"}, 750, function() {
+    $('.gallery .slide').animate({left: "-=100%"}, 750, function() {
       isAnimating = false;
       if ($('.last').css('left') === "0px") {
         $('.next').addClass('is-disabled');
@@ -105,34 +230,6 @@ $(document).ready(function() {
       $('.previous').removeClass('is-disabled');
     }
   });
-
-/*==========================================================================
-  ON INITIAL PAGE LOAD
-==========================================================================*/
-
-  var initialLoad = function() {
-    var hash = window.location.hash.substring(1);
-    $('#main-header .' + hash).addClass('is-active');
-    $('#main-header .' + hash + ' a').attr('disabled', 'disabled');  // Prevents further clicks on active link in IE
-    $.get("subPagesDocument.html", function(data) {
-      $subPagesDocument = $(data);
-      // Wait until the HTML document with all the sub-pages has been loaded and assigned to a variable
-      loadPageContent(hash, function() {
-        $guts.toggleClass('slideUp slideDown');
-        setTimeout(function(){$('.content').toggleClass('fadeOut fadeIn');}, slideTransitionLength);  // $guts slide transition takes 600ms
-      });
-    });
-  };
-
-  // If the initial URL contains a hash, load that page and go to it
-  if (window.location.hash ) {
-    initialLoad();
-  }
-  // Or else just make 'entrance' the hash, and load n' go there
-  else {
-    window.location.hash = "entrance";
-    initialLoad();
-  }
 
 /*==========================================================================
   NAVIGATION
@@ -164,16 +261,39 @@ $(document).ready(function() {
   STATES
 ==========================================================================*/
 
+  // If the user hasn't consented or them saying ok wasn't stored on this computer, show them the warning
+  if (sessionStorage.wtfdrinkPermission !== true) {
+    $guts.on('click', '.portfolio-item.wtfdrink a', function(e) {
+      $('#wtf-overlay').show();
+      e.preventDefault();
+    });
+  }
+
+  // If they click no and don't want to see all the F-Bombs, store their answer in sessionStorage and return them to the portfolio
+  $('#no-way').click(function() {
+    sessionStorage.setItem('wtfdrinkPermission', false);
+    $('#wtf-overlay').hide();
+  });
+  // If they click yes and want to see all that F-Bomb goodness, store that answer also and send them on their merry little way. Oh and also unbind the event that asks for permission to enter the wtfdrink subgallery.
+  $('#yes-way').click(function() {
+    sessionStorage.setItem('wtfdrinkPermission', true);
+    $('#wtf-overlay').hide();
+    window.location.hash = 'wtfdrink';
+    $guts.off('click', '.portfolio-item.wtfdrink a');
+  });
+
   $(window).on('hashchange', function() {
     // Collect information about current states
     var newHash = window.location.hash.substring(1);  // Where we are headed...
     // Are we going into a sub gallery?
     if ($.inArray(newHash, subGalleries) >= 0) {
       $('#main-header .nav-item').removeClass('is-active');
+      $('#main-header a').removeAttr('disabled');
       $('#portfolio').toggleClass('fadeIn fadeOut');
       setTimeout(function() {
         loadPageContent(newHash, function() {
           $('.content').toggleClass('fadeIn fadeOut');
+          $('html, body').animate({ scrollTop: 0 }, 400);
         });
       }, fadeTransitionLength);
     }
@@ -183,18 +303,22 @@ $(document).ready(function() {
       setTimeout(function(){
         loadPageContent(newHash, function() {
           $('.content').toggleClass('fadeIn fadeOut');
+          $('html, body').animate({ scrollTop: 0 }, 400);
         });
       }, fadeTransitionLength);
     // Just moving from one content section to another
     } else {
       $('.content').toggleClass('fadeIn fadeOut');
-      setTimeout(function(){$guts.toggleClass('slideDown slideUp');}, fadeTransitionLength);  // $('.content') fade transition takes 200ms
+      setTimeout(function(){$guts.toggleClass('slideDown slideUp');}, fadeTransitionLength);
       setTimeout(function() {
         loadPageContent(newHash, function() {
           $guts.toggleClass('slideUp slideDown');
-          setTimeout(function(){$('.content').toggleClass('fadeOut fadeIn');}, slideTransitionLength);  // $guts slide transition takes 600ms
+          setTimeout(function(){
+            $('.content').toggleClass('fadeOut fadeIn');
+            $('html, body').animate({ scrollTop: 0 }, 400);
+          }, slideTransitionLength);
         });
-      }, fadeTransitionLength + slideTransitionLength);  // 200ms for the fade + 600ms for the slide
+      }, fadeTransitionLength + slideTransitionLength);
     }
   });
 
@@ -248,5 +372,4 @@ $(document).ready(function() {
       $('.status-message, .failure-message').fadeIn().delay(messageDelay).fadeOut();  // Redisplay the failure form!
     }
   }
-
 });
