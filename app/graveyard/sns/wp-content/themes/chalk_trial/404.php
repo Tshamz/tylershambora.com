e117e9f3b48f78af9596af77275baa195d36954b
tshamz <tyler@theshamboras.com>
2014-07-27 02:55:20 -0700

    removed some dirs from repo

--- app/graveyard/sns/wp-content/themes/chalk_trial/404.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<div class="wrapper">
-	<h3><?php _e( '404: Page Not Found', 'chalk' ); ?></h3>
-	<p><?php _e( 'We are terribly sorry, but the URL you typed no longer exists. It might have been moved or deleted. Try searching the site:', 'chalk' ); ?></p>
-	<?php get_search_form(); ?>
-</div>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
--- app/graveyard/sns/wp-content/themes/chalk_trial/index.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<div class="wrapper">
-	<?php get_template_part( '_loop' ); ?>
-	<?php get_template_part( '_index-footer' ); ?>
-</div>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
