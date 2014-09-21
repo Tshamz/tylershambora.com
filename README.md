e8d959dcbf4e1c4c6d6ec941909563abd829dded
Tyler Shambora <tyler@theshamboras.com>
2014-09-21 14:41:04 -0700

    updates stuf

--- README.md
+++ README.md
@@ -8,7 +8,13 @@ Check out [tylershambora.com](http://tylershambora.com) for an up to date, live 
 
 No full page reloads, loads new content in via ajax, most of the animations are done using CSS3 transitions/animations, stylesheets are written using SCSS, fully responsive and built mobile-first, modular javascript, uses Grunt to: work locally with livereload, concatenate and minify CSS & JavaScript, optimize images, and create a ready to go dist directory.
 
-Repository doesn't include "dist/" directory with all the renamed, minimized, concatenated files. Also not included are the compiled .css files. You'll only find .scss partials here.
+**NOTE:** This repository is for review purposes only, it's not meant to be cloned. This means it doesn't include a "dist/" directory where you would find:
+* compressed image files
+* compressed font files
+* compiled & minified .css files (main.css, enhanced.css)
+* concatenated & minified .js modules (main.js)
+* concatenated & minified .js vendor files (jquery.js, modernizr.js ~> vendor.js)
+* minified versions of feature-test.js, gallery-info.json
 
 ## Goals/TODO ##
 
@@ -24,8 +30,9 @@ Repository doesn't include "dist/" directory with all the renamed, minimized, co
 *  ~~Rewrite all the copy associated with the gallery items so they are the correct length~~
 *  ~~Build a better light box~~
 *  ~~Look into more about support for pointer-events~~
-*  Improve a11y
 *  ~~Rivers in text~~
 *  ~~Vertically align gallery images in portrait mode~~
+*  Improve a11y
+*  Set up filerev to rename feature-test.js, gallery-info.json, to uncache the new build files
 
 kbye.
--- app/scripts/feature-test.js
+++ app/scripts/feature-test.js
@@ -5,17 +5,17 @@
 
   var unsupportedFeatures = [];
   var fullFeatureNameMap = {
-    apng: '.apng File Type',  // this is a test
+    // apng: '.apng File Type',  // this is a test
     backgroundsize: 'CSS Background-Size',
-    battery: 'Battery API',  // this is also a test
+    // battery: 'Battery API',  // this is also a test
     borderradius: 'CSS Border-Radius',
     boxshadow: 'CSS Box-Shadow',
     boxsizing: 'CSS Box-Sizing',
-    contextmenu: 'HTML5 Context Menus',  // also a test, supported in Firefox
-    css2Dtransforms: 'CSS 2D Transforms',
-    css3Dtransforms: 'CSS 3D Transforms',
+    // contextmenu: 'HTML5 Context Menus',  // also a test, supported in Firefox
+    csstransforms2d: 'CSS 2D Transforms',
+    csstransforms3d: 'CSS 3D Transforms',
     cssanimations: 'CSS Animations',
-    csspositionsticky: 'CSS Position: Sticky;',  // test
+    // csspositionsticky: 'CSS Position: Sticky;',  // test
     csstransitions: 'CSS Transitions',
     flexbox: 'CSS Flex-Box',
     fontface: 'CSS @font-face',
@@ -42,7 +42,7 @@
       featureNames += '-' + t + '-  ';
       li += '<li>' + t + '</li>';
     });
-    var noSupportModal = '<div id="no-support"><div class="no-support-inner"><div class="no-support-text"><h2 class="no-support-header">You are using an outdated browser!</h2><p class="unsupported-features-lead-in">The following features are unsupported or turned off:</p><ul id="unsupported-features">' + li + '</ul><p>and they may have robbed you of what might have been arguably your most exciting experience on the Internet to date. <i>(It\'s also possible you\re using Safari (or Mobile Safari) and have private browsing enabled. Turning off private browsing and reloading should fix everything.)</i></i></p><p>A list of today\'s most fabulous and exciting browsers can be found below.</p></div><div class="no-support-downloads"><span class="download chrome"><a href="https://www.google.com/chrome/browser/" target="_blank"><img src="images/icons/chrome.png" /></a><em>Google Chrome</em></span><span class="download firefox"><a href="https://www.mozilla.org/en-US/firefox/new/?utm_source=firefox-com&utm_medium=referral" target="_blank"><img src="images/icons/firefox.png" /></a><em>Mozilla Firefox</em></span><span class="download safari"><a href="http://www.apple.com/safari/" target="_blank"><img src="images/icons/safari.png" /></a><em>Apple Safari</em></span><span class="download opera"><a href="http://www.opera.com/" target="_blank"><img src="images/icons/opera.png" /></a><em>Opera... Opera</em></span></div></div></div><style>html,body{height:100%;overflow-y:hidden;}</style>';
+    var noSupportModal = '<div id="no-support"><div class="no-support-inner"><div class="no-support-text"><h2 class="no-support-header">You are using an outdated browser!</h2><p class="unsupported-features-lead-in">The following features are unsupported or turned off:</p><ul id="unsupported-features">' + li + '</ul><p>and they may have robbed you of what might have been arguably your most exciting experience on the Internet to date. <i>(It\'s also possible you\'re using Safari (or Mobile Safari) and have private browsing enabled. Turning off private browsing and reloading should fix everything.)</i></p><p>A list of today\'s most fabulous and exciting browsers can be found below.</p></div><div class="no-support-downloads"><span class="download chrome"><a href="https://www.google.com/chrome/browser/" target="_blank"><img src="images/icons/browser/chrome.png" /></a><em>Google Chrome</em></span><span class="download firefox"><a href="https://www.mozilla.org/en-US/firefox/new/?utm_source=firefox-com&utm_medium=referral" target="_blank"><img src="images/icons/browser/firefox.png" /></a><em>Mozilla Firefox</em></span><span class="download safari"><a href="http://www.apple.com/safari/" target="_blank"><img src="images/icons/browser/safari.png" /></a><em>Apple Safari</em></span><span class="download opera"><a href="http://www.opera.com/" target="_blank"><img src="images/icons/browser/opera.png" /></a><em>Opera... Opera</em></span></div></div></div><style>html,body{height:100%;overflow-y:hidden;}</style>';
     $('body').append(noSupportModal);
     $('#no-support').show();
     ga('send', 'event', 'Compatibility', 'Load', 'Unsupported Browser: ' + featureNames);
